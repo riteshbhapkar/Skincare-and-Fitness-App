6d3667be22c19c14ce1e67dc1e7c320911b0aaca
Ritesh Bhapkar <102727896+riteshbhapkar@users.noreply.github.com>
2024-12-06 20:41:51 +0530

    Add files via upload

---
+++ src/middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+export const config = {
+  matcher: "/integrations/:path*",
+};
+
+export function middleware(request) {
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-createxyz-project-id", "3dc6dda5-ce64-4b26-886c-ed31303f37a7");
+
+  request.nextUrl.href = `https://www.create.xyz/${request.nextUrl.pathname}`;
+
+  return NextResponse.rewrite(request.nextUrl, {
+    request: {
+      headers: requestHeaders,
+    },
+  });
+}
